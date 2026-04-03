8c4fdfe924757abf487b69e59a05f53005673d5a
Jash <jashb1268@gmail.com>
2026-04-02 19:16:31 -0600

    3 arcs does not work

--- ASEN 6950/Code/DF_mat.m
+++ ASEN 6950/Code/DF_mat.m
@@ -1,5 +1,5 @@
 % function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_4_des)
-function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_2_des)
+function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_2_des, x_3_des)
     % Modified constraint DF matrix
 
     % Get mass ratio of system
@@ -16,40 +16,44 @@ function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_2_des)
     m_2_0 = V(14);
     Dt2 = V(15);
     uhat_2 = V(16:18);
-    % x_3_0 = V(19:24);
-    % m_3_0 = V(25);
-    % Dt3 = V(26);
-    % uhat_3 = V(27:29);
+    x_3_0 = V(19:24);
+    m_3_0 = V(25);
+    Dt3 = V(26);
+    uhat_3 = V(27:29);
     % x_4_0 = V(30:35);
     % Dt4 = V(36);
 
     x_1_f = statef_V(1:6);
     x_2_f = statef_V(8:13);
     m_2_f = statef_V(14);
-    % x_3_f = statef_V(19:24);
-    % m_3_f = statef_V(25);
+    x_3_f = statef_V(19:24);
+    m_3_f = statef_V(25);
     % x_4_f = statef_V(30:35);
 
     phi0 = reshape(eye(6), [36, 1]); % Initial phi is identity
 
     x1_0 = [V(1:6); phi0];
     x2_0 = [V(8:13); m_2_0; phi0];
-    % x3_0 = [V(15:20); m_3_0; phi0];
+    x3_0 = [V(19:24); m_3_0; phi0];
     % x4_0 = [V(22:27); phi0];
 
     % Define functions
     init_fun = @(t,state)CR3BP_full_mass(state, mu, T, Isp, uhat_2, t_star, l_star, m_2_0);
-    % final_fun = @(t,state)CR3BP_full_mass(state, mu, T, Isp, uhat_3, t_star, l_star, m_3_0);
+    final_fun = @(t,state)CR3BP_full_mass(state, mu, T, Isp, uhat_3, t_star, l_star, m_3_0);
 
     % Non-dim thrust
     f = (T*t_star^2)/(l_star*M_sc_0);
     % Non-dim mass
     m_2 = m_2_0/M_sc_0;
+    m_3 = m_3_0/M_sc_0;
     % acceleration due to low thrust
     % a_lt_2 = -f/m_2*uhat_2;
     a_lt_2 = -f/m_2;
+    a_lt_3 = -f/m_3;
     a_lt_mat_2 = [a_lt_2, 0, 0; 0, a_lt_2, 0; 0, 0, a_lt_2];
+    a_lt_mat_3 = [a_lt_3, 0, 0; 0, a_lt_3, 0; 0, 0, a_lt_3];
     a_lt_dot_2 = -f/m_2^2*uhat_2;
+    a_lt_dot_3 = -f/m_3^2*uhat_3;
 
     % m_3 = m_3_0/M_sc_0;
     % a_lt_3 = -f/m_3*uhat_3;
@@ -58,31 +62,33 @@ function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_2_des)
 
     [~, x1_out] = ode113(@(t,state)CR3BP_full(state, mu), [0, Dt1], x1_0, options);
     [~, x2_out] = ode113(init_fun, [0 Dt2], x2_0, options);
-    % [~, x3_out] = ode113(final_fun, [0 Dt3], x3_0, options);
+    [~, x3_out] = ode113(final_fun, [0 Dt3], x3_0, options);
     % [~, x4_out] = ode113(@(t,state)CR3BP_full(state, mu), [0, Dt4], x4_0, options);
     
     x1_f = x1_out(end, :)';
     x2_f = x2_out(end, :)';
-    % x3_f = x3_out(end, :)';
+    x3_f = x3_out(end, :)';
     % x4_f = x4_out(end, :)';
     xdot_1_0 = CR3BP(x_1_0, mu);
     xdot_1_f = CR3BP(x1_f, mu);
     xdot_2_f = CR3BP_with_non_dim_mass(x2_f, mu, T, Isp, uhat_2, t_star, l_star, m_2_0);
     mdot_2_f = xdot_2_f(7);
     xdot_2_f = xdot_2_f(1:6);
-    % xdot_3_f = CR3BP_with_non_dim_mass(x3_f, mu, T, Isp, uhat_3, t_star, l_star, m_3_0);
-    % mdot_3_f = xdot_3_f(7);
-    % xdot_3_f = xdot_3_f(1:6);
+    xdot_3_f = CR3BP_with_non_dim_mass(x3_f, mu, T, Isp, uhat_3, t_star, l_star, m_3_0);
+    mdot_3_f = xdot_3_f(7);
+    xdot_3_f = xdot_3_f(1:6);
     % xdot_4_f = CR3BP(x4_f, mu);
-    xdot_1_des = CR3BP(x_1_des, mu);
+    % xdot_1_des = CR3BP(x_1_des, mu);
     % xdot_4_des = CR3BP(x_4_des, mu);
+
+    mdot = -(f*l_star)/(Isp*9.80665e-3*t_star);
 
     phi_row_1 = x1_f(7:end);
     phi_mat_1 = reshape(phi_row_1, [6,6])';
     phi_row_2 = x2_f(8:end);
     phi_mat_2 = reshape(phi_row_2, [6,6])';
-    % phi_row_3 = x3_f(8:end);
-    % phi_mat_3 = reshape(phi_row_3, [6,6])';
+    phi_row_3 = x3_f(8:end);
+    phi_mat_3 = reshape(phi_row_3, [6,6])';
     % phi_row_4 = x4_f(7:end);
     % phi_mat_4 = reshape(phi_row_4, [6,6])';
 
@@ -91,8 +97,8 @@ function out = DF_mat(V, statef_V, options, system_params, x_1_des, x_2_des)
     %         zeros(7,18), [phi_mat_3; zeros(1,6)], [zeros(3,1); a_lt_dot_3; 0], [xdot_3_f; 0], [zeros(3); a_lt_mat_3; [2*uhat_3(1), 2*uhat_3(2), 2*uhat_3(3)]], [-eye(6); zeros(1,6)], zeros(7,1);
     %         [eye(6); zeros(6)], [xdot_1_0 - xdot_1_des; zeros(6,1)], zeros(12,22), [zeros(6); eye(6)], [zeros(6,1); xdot_4_f - xdot_4_des]];
 
-    out = [phi_mat_1, xdot_1_f, -eye(6), zeros([6,5]);
-            % eye(6), zeros(6,1), zeros(6,11);
-            zeros(8,7), [phi_mat_2; zeros([2,6])], [zeros([3,1]); a_lt_dot_2; 0; 1], [xdot_2_f; 0; 0], [zeros(3); a_lt_mat_2; [2*uhat_2(1), 2*uhat_2(2), 2*uhat_2(3)]; zeros(1,3)]];
+    out = [phi_mat_1, xdot_1_f, -eye(6), zeros(6,5), zeros(6,11);
+            zeros(9,7), [phi_mat_2; zeros([3,6])], [zeros([3,1]); a_lt_dot_2; 0; 1; 1], [xdot_2_f; 0; 0; mdot], [zeros(3); a_lt_mat_2; [2*uhat_2(1), 2*uhat_2(2), 2*uhat_2(3)]; zeros(2,3)], [-eye(6), zeros(6,5); zeros(2,11); zeros(1,6), -1, zeros(1,4)];
+            zeros(7,18), [phi_mat_3; zeros(1,6)], [zeros(3,1); a_lt_dot_3; 0], [xdot_3_f; 0], [zeros(3); a_lt_mat_3; [2*uhat_3(1), 2*uhat_3(2), 2*uhat_3(3)]]];
 
 end
--- ASEN 6950/Code/F.m
+++ ASEN 6950/Code/F.m
@@ -1,19 +1,20 @@
 % function out = F(state0, statef, x_1_des, x_4_des)
-function out = F(state0, statef, x_1_des, x_2_des, init_mass)
+function out = F(state0, statef, x_1_des, x_2_des, x_3_des, init_mass)
     % Modified Constraint Vector
     F1 = statef(1:6) - state0(8:13); % x1f - x20
             % state0(1:6) - x_1_des]; % x10 - x1des
-    F2 = [statef(8:13) - x_2_des; % x2f - x2des
+    F2 = [statef(8:13) - state0(19:24); % x_2_f - x_3_0
             norm(state0(16:18))^2 - 1; % ||uhat2||^2 - 1
-            state0(14) - init_mass]; % m_2_0 - init_mass
-    % F2 = [statef(8:13) - state0(19:24); % x2f - x30
-    %       statef(14) - state0(25); % m2f - m30
-    %       norm(state0(16:18)) - 1]; % ||uhat2||^2 - 1
+            state0(14) - init_mass; % m_2_0 - init_mass
+            statef(14) - state0(25)]; % m_2_f - m_3_0
+    F3 = [statef(19:24) - x_3_des; % x_3_f - x_3_des
+            norm(state0(27:29))^2 - 1]; % ||uhat3||^2 - 1
+    
     % F3 = [statef(19:24) - state0(30:35); % x3f - x40
     %       norm(state0(27:29)) - 1]; % ||uhat3||^2 - 1
     % F4 = [statef(1:6) - x_1_des; % x10 - x1des
     %         statef(30:35) - x_4_des]; % x4f - x4des
     
     % out = [F1; F2; F3; F4];
-    out = [F1; F2];
+    out = [F1; F2; F3];
 end
